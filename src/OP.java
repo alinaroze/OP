6ccde738b1c6df1bda81173383d816852ae148f4
Alina Rozenbaum <alinaroze94@gmail.com>
2016-02-09 11:25:27 -0800

    Full Upload

---
+++ src/OP.java
@@ -0,0 +1,20 @@
+
+public class OP{
+
+
+
+public static void main(String[] args){
+
+
+
+String wut = "Fried Omlet";
+
+
+
+String lul = "Pegasus";
+
+
+
+System.out.println("+wut.charAt(5)+lul.charAt(0)+" "+wut.charAt(2)+lul.charAt(4)+" "+lul.charAt(3)+" "+wut.charAt(0)+lul.charAt(3)+lul.charAt(2));
+}
+}
